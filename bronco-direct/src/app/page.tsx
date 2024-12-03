6176697751fb5ec1f781b938dbda0a56a0335887
stephaniePocci <svpocci@cpp.edu>
2024-12-02 18:01:59 -0800

    Add class management pages and update global styles

--- bronco-direct/src/app/page.tsx
+++ bronco-direct/src/app/page.tsx
@@ -1,19 +1,29 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[var(--font-news-gothic-bold)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <ul className="list-inside text-sm text-center sm:text-left font-[var(--font-news-gothic-bold)]">
           <li className="mb-2">
-            <button>Swap Classes</button>
+            <Link href="/swap">
+              <button>Swap Classes</button>
+            </Link>
           </li>
           <li className="mb-2">
-            <button>Add Classes</button>
+            <Link href="/add">
+              <button>Add Classes</button>
+            </Link>
           </li>
           <li className="mb-2">
-            <button>Drop Classes</button>
+            <Link href="/drop">
+              <button>Drop Classes</button>
+            </Link>
           </li>
           <li className="mb-2"> 
-            <button>Search Classes</button>
+            <Link href="/search">
+              <button>Search Classes</button>
+            </Link>
           </li>
         </ul>
       </main>
---
+++ bronco-direct/src/app/swap/page.tsx
@@ -0,0 +1,9 @@
+export default function SwapClasses() {
+    return (
+      <div>
+        <h1 className="font-[var(--font-news-gothic-bold)">Swap Classes</h1>
+        <p>This is the page where users can swap classes.</p>
+      </div>
+    );
+}
+  
